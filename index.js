b612be97d2f03d13f8e7734b8862a4f3a29b1a92
Shajib-Dv <mdsojeeb242@gmail.com>
2023-05-02 00:40:54 +0600

    data created

--- index.js
+++ index.js
@@ -5,10 +5,34 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
+const chefs = require("./data/chefs.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server is running on port 5000");
+});
+
+//all data
+app.get("/chefs", (req, res) => {
+  res.send({ status: true, data: chefs });
+});
+
+// filtered by origin
+app.get("/origins/:origin", (req, res) => {
+  const origin = req.params.origin;
+  const originData =
+    chefs.filter((ch) =>
+      ch.origin.toLowerCase().includes(`${origin.toLowerCase()}`)
+    ) || [];
+  res.send(originData);
+});
+
+//single data by id
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const singleData = chefs.find((ch) => ch._id == id) || {};
+  res.send(singleData);
 });
 
 app.listen(port, () => {
